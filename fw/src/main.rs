3b534811a3548be1b22f399f837b5a1f8b23301e
Craig Bishop <craig@craigjb.com>
2024-09-02 23:09:34 -0700

    Rust running on VexRiscv

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+use defmt_rtt as _;
+use panic_halt as _;
+use riscv_rt::entry;
+
+fn delay(cycles: u32) {
+    for _ in 0..cycles {
+        unsafe {
+            asm!("nop");
+        }
+    }
+}
+
+fn set_leds(mask: u32) {
+    unsafe {
+        *(0xf0000000 as *mut u32) = mask;
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    defmt::println!("Hello from RISC-V!");
+    let mut mask = 0x80;
+    loop {
+        set_leds(mask);
+        mask >>= 1;
+        if mask == 0 {
+            mask = 0x80;
+        }
+        delay(600000);
+        defmt::println!("Hello from RISC-V!");
+    }
 }
