ab7310ba8e9ff81c0b32dbbea6868552328c8a8a
Craig Bishop <craig@craigjb.com>
2024-09-06 10:55:08 -0700

    Use slab-pac

--- fw/slabware/src/main.rs
+++ fw/slabware/src/main.rs
@@ -1,31 +1,19 @@
 #![no_std]
 #![no_main]
 
-use core::arch::asm;
 use defmt_rtt as _;
 use panic_halt as _;
+use riscv::asm::delay;
 use riscv_rt::entry;
-
-fn delay(cycles: u32) {
-    for _ in 0..cycles {
-        unsafe {
-            asm!("nop");
-        }
-    }
-}
-
-fn set_leds(mask: u32) {
-    unsafe {
-        *(0xf0000000 as *mut u32) = mask;
-    }
-}
 
 #[entry]
 fn main() -> ! {
+    let peripherals = slab_pac::Peripherals::take().unwrap();
+
     defmt::println!("Hello from RISC-V!");
     let mut mask = 0x80;
     loop {
-        set_leds(mask);
+        peripherals.leds.data().write(|w| unsafe { w.bits(mask) });
         mask >>= 1;
         if mask == 0 {
             mask = 0x80;
